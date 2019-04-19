0ffbcc40f69d31b6c10f8d35f86987515894cdee
Joe <joe@astucc.io>
2019-04-19 19:35:11 -0400

    feat: Update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 const Header = () => {
   return (
-    <div>header</div>
+    <header>
+      <h1>Joe Astuccio</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog/">Blog</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/contact/">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Layout from "../components/layout"
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>Joe Astuccio</h1>
       <h2>
         Web Developer, Sailor, Stargazer, Hockey Player, and all around fun guy... but my absolute favorite thing is being a dad.
       </h2>
