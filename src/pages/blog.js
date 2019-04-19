8479ad5085ed6fb0bbe6dd3aa598370e5e4f7a44
Joe <joe@astucc.io>
2019-04-19 15:04:34 -0400

    feat: create layout component

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BlogPage() {
+  return (
+    <>
+    <h1>Blog</h1>
+    <p>posts</p>
+    </>
+  )
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/layout"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Joe Astuccio</h1>
+      <h2>
+        Web Developer, Sailor, Stargazer, Hockey Player, and all around fun guy... but my absolute favorite thing is being a dad.
+      </h2>
+      <p>Need a developer?<Link to="/contact/">Contact me.</Link></p>
+    </Layout>
+  )
+}
+
+export default IndexPage
