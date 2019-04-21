85c6c1ead7d8956fe4842b01adad52dab273cddc
Joe <joe@astucc.io>
2019-04-21 16:45:11 -0400

    refactor: Convert color variables to use HSL

---
+++ src/utilities/colors.js
@@ -0,0 +1,7 @@
+export const white = "hsl(0, 0%, 99%)" //#fcfcfc
+
+export const black = "hsl(0, 0%, 18%)" //#2f2f2f
+
+export const gray = "hsl(0, 0%, 60%)" //#999999"
+export const gray_dark = "hsl(0, 0%, 40%)"//#666666
+export const gray_dark_2 = "hsl(0, 0%, 20%)" //#333333
