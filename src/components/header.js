d641373e96d8c8936432974e61d0007b3f0a1034
Joe <joe@astucc.io>
2019-04-21 18:17:17 -0400

    feat: Add styling to header and layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,64 @@
-import React from 'react'
-import {Link} from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
+
+import styled from "styled-components"
+import { black, gray, gray_dark, gray_dark_2 } from "../utilities"
+
+const NavItem_Link = styled(props => (
+  <Link activeClassName="active" {...props} />
+))`
+  color: ${gray};
+  font-size: 0.9rem;
+  margin-right: 1.3rem;
+  text-decoration: none;
+
+  &:hover {
+    color: ${gray_dark};
+  }
+
+  &.active {
+    color: ${gray_dark_2};
+  }
+`
+
+const Title_Link = styled(props => <Link {...props} />)`
+  color: ${black};
+  font-size: 3rem;
+  text-decoration: none;
+`
+
+const Styled_header = styled.header`
+  padding: 1rem 0 3rem;
+`
+const Nav_ul = styled.ul`
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+`
 
 const Header = () => {
   return (
-    <header>
-      <h1>Joe Astuccio</h1>
+    <Styled_header>
+      <h1>
+        <Title_Link to="/">Joe Astuccio</Title_Link>
+      </h1>
       <nav>
-        <ul>
+        <Nav_ul>
           <li>
-            <Link to="/">Home</Link>
+            <NavItem_Link to="/">Home</NavItem_Link>
           </li>
           <li>
-            <Link to="/blog/">Blog</Link>
+            <NavItem_Link to="/blog/">Blog</NavItem_Link>
           </li>
           <li>
-            <Link to="/about/">About</Link>
+            <NavItem_Link to="/about/">About</NavItem_Link>
           </li>
           <li>
-            <Link to="/contact/">Contact</Link>
+            <NavItem_Link to="/contact/">Contact</NavItem_Link>
           </li>
-        </ul>
+        </Nav_ul>
       </nav>
-    </header>
+    </Styled_header>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import GlobalStyles from '../global-styles'
+import React from "react"
+import GlobalStyles from "../global-styles"
 import Header from "./header"
 import Footer from "./footer"
 
-const Layout = ( props ) => {
+import styled from "styled-components"
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 750px;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const Content = styled.div`
+  flex-grow: 1;
+`
+
+const Layout = props => {
   return (
-    <div>
-    <GlobalStyles />
-      <Header />
-      {props.children}
+    <Container>
+      <GlobalStyles />
+      <Content>
+        <Header />
+        {props.children}
+      </Content>
       <Footer />
-    </div>
+    </Container>
   )
 }
 
