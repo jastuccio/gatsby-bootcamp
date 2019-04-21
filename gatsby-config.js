65182c056758bbeb04a82173b35e23d4823fc841
Joe <joe@astucc.io>
2019-04-21 18:16:07 -0400

    bug: move gatsby-config.js to the correct location
    bug: Add index file to utilities

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Bootcamp',
+    description: 'A Gatsby tutorial by Andrew Mead',
+    author:'Joe Astuccio',
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
