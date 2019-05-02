87b9ee4d0ccb17b2319024990b5228284271b434
Joe <joe@astucc.io>
2019-05-02 06:15:13 -0400

    feat: Show Images in Blog Posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,33 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'src',
         path: `${__dirname}/src/`,
       },
     },
-    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
   ],
 }
