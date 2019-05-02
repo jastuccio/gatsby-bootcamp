5c70a66284171a66af2b584fc936575e96377f69
Joe <joe@astucc.io>
2019-05-02 10:46:55 -0400

    feat: style the posts list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,19 +2,41 @@ import React from 'react'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 
 import styled from 'styled-components'
+import { black, gray200, gray300, gray700 } from '../utilities'
+
 import Layout from '../components/layout'
 
+// .posts <ol>
 const PostList = styled.ol`
+  list-style-type: none;
   margin: 0;
-  color: blue;
 `
 
-const StyledH2 = styled.h2`
-  color: yellow;
+// .post <li>
+const PostItem = styled.li``
+
+// .post a
+const BlogPostLink = styled(props => <Link {...props} />)`
+  margin: 1rem 0;
+  background: ${gray200};
+  color: ${black};
+  display: block;
+  padding: 1rem;
+  text-decoration: none;
+
+  &:hover {
+    background: ${gray300};
+  }
 `
 
-const StyledLink = styled(props => <Link {...props} />)`
-  color: aqua;
+const H2 = styled.h2`
+  margin-bottom: 0;
+`
+
+const P = styled.p`
+  color: ${gray700};
+  font-size: 0.8rem;
+  font-style: italic;
 `
 
 const BlogPage = () => {
@@ -40,14 +62,12 @@ const BlogPage = () => {
       <h1>Blog</h1>
       <PostList>
         {data.allMarkdownRemark.edges.map(edge => (
-          <>
-            <StyledLink to={`/blog/${edge.node.fields.slug}`}>
-              <li>
-                <StyledH2>{edge.node.frontmatter.title}</StyledH2>
-              </li>
-              <p>{edge.node.frontmatter.date}</p>
-            </StyledLink>
-          </>
+          <BlogPostLink to={`/blog/${edge.node.fields.slug}`}>
+            <PostItem>
+              <H2>{edge.node.frontmatter.title}</H2>
+              <P>{edge.node.frontmatter.date}</P>
+            </PostItem>
+          </BlogPostLink>
         ))}
       </PostList>
     </Layout>
